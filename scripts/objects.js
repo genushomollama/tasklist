b20df6ca689f95fb8db68fd95028d2ad138cc198
genushomollama <gregorylitmannavarro@gmail.com>
2020-12-31 15:57:23 -0800

    Initial files - skeleton
    Mostly boilerplate.

---
+++ scripts/objects.js
@@ -0,0 +1,16 @@
+class Task {
+  constructor(name, hr, min, sec) {
+    this.name = name;
+    this.hour = hr;
+    this.minute = min;
+    this.second = sec;
+  }
+
+  // Todo getters and setters
+}
+
+class TaskList {
+  constructor() {
+    // this.tasks = 
+  }
+}
